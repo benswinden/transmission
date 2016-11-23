1fd67662917c83aa58fc897654b40707db2f67d8
ben <swinden.ben@gmail.com>
2016-11-22 22:38:27 -0500

    : wireframe shader : creating particles from vertexes

---
+++ transmission/Assets/_Scripts/Emitter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Emitter : MonoBehaviour {
+
+    public GameObject meshObject;
+    public GameObject obj;
+
+    [Space]
+
+    public float waitTime = 0.5f;
+
+
+    List<Vector3> vertexList = new List<Vector3>();
+
+
+    void Awake() {
+
+        var verticeList = meshObject.GetComponent<MeshFilter>().mesh.vertices;
+
+        foreach (var item in verticeList) {
+
+            vertexList.Add( meshObject.transform.TransformPoint(item));
+        }
+
+        StartCoroutine("createObject");
+    }
+
+    IEnumerator createObject() {
+
+        yield return new WaitForSeconds(waitTime);
+
+        Instantiate(obj, vertexList[Random.Range(0, vertexList.Count)], Quaternion.identity);
+
+        StartCoroutine("createObject");
+    }
+
+}
---
+++ transmission/Assets/_Scripts/Particle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle : MonoBehaviour {
+
+
+    public float force = 100;
+
+    Rigidbody rigidbody;
+
+    void Awake() {
+
+        rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(transform.up * force, ForceMode.Impulse);
+    }
+
+}
