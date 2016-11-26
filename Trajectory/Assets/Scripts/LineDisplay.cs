f5168d81b215faf573edc07d59ad9bb5d70359ef
James Paterson <j@presstube.com>
2016-11-25 22:14:38 -0500

    added tri asset test in james-playbin

---
+++ Trajectory/Assets/Scripts/LineDisplay.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Vectrosity;
+ 
+public class LineDisplay : MonoBehaviour {
+
+	//line drawing
+	private VectorLine Line;
+	private List<Vector3> LinePoints;
+
+	//width of line
+	public float LineWidth = 1f;
+
+	void Awake() {
+		//initial line points
+		LinePoints = new List<Vector3>() {Vector3.zero, Vector3.zero};
+		//create vectrosity line
+		Line = new VectorLine("Line", LinePoints, LineWidth, LineType.Continuous);
+	}
+ 
+	public void DisplayLine(List<Vector3> linePoints) {
+		//clear line list
+		LinePoints.Clear();
+		//add new points to line list
+		for(int i = 0; i < linePoints.Count; i++){
+			LinePoints.Add(linePoints[i]);
+		}
+		//create line if it was destroyed
+		if(Line == null){
+			Line = new VectorLine("Line", LinePoints, LineWidth, LineType.Continuous);
+			Line.active = true;
+		}
+		//draw line
+		Line.Draw3D();
+
+	}
+
+	public void ClearLine() {
+		//clear point list
+		LinePoints.Clear();
+		if (Line != null) {
+			Line.Draw3D();
+			Line.active = false;
+		}
+		//kill line
+		VectorLine.Destroy(ref Line);
+	}
+
+	public bool LineActive{
+		get{
+			return LinePoints.Count > 0;
+		}
+	}
+
+ }
---
+++ Trajectory/Assets/Scripts/Trajectory.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//trigger down state
+public enum Controller {
+	Controller1,
+	Controller2,
+	None
+}
+
+public class Trajectory : MonoBehaviour {
+
+	public GameObject TrajectoryTrackPrefab;
+
+	//offset from center of controller to draw line (pen tip)
+	public Vector3 DrawOffset;
+
+	//vr manager
+	private GameObject VRManagerGO;
+	private VRManager VRManager;
+
+	//transform
+	private Transform TR;
+
+	//recorder
+	private TrackRecorder Recorder;
+
+	//current track
+	private GameObject CurrentTrack1;
+	private GameObject CurrentTrack2;
+	private TrajectoryTrack CurrentTrajectoryTrack1;
+	private TrajectoryTrack CurrentTrajectoryTrack2;
+
+	//all tracks
+	private List<TrajectoryTrack> TrajectoryTracks;
+
+	void Awake () {
+		//cache references
+		VRManagerGO = GameObject.Find("VRManager");
+		VRManager = VRManagerGO.GetComponent<VRManager>();
+		TR = GetComponent<Transform>();
+		TrajectoryTracks = new List<TrajectoryTrack>();
+	}
+
+	void Start() {
+		//create recorder, listen for events
+		Recorder = new TrackRecorder();
+		Recorder.Init(DrawOffset);
+		Recorder.OnRecordTrackStart += CreateNewTrack;
+		Recorder.OnRecordTrackFinished += TrackRecordFinished;
+		Recorder.OnRecordingTrack += UpdateCurrentTrack;
+		//hook up vr inputs to recorder
+		VRManager.OnController1TriggerDown += Recorder.OnController1TriggerDown;
+		VRManager.OnController1TriggerUp += Recorder.OnController1TriggerUp;
+		VRManager.OnController2TriggerDown += Recorder.OnController2TriggerDown;
+		VRManager.OnController2TriggerUp += Recorder.OnController2TriggerUp;
+		VRManager.OnControllerPositionUpdate += Recorder.OnControllerPositionUpdate;
+		//VRManager.OnController1GripDown += OnController1GripDown;
+		//VRManager.OnController2GripDown += OnController2GripDown;
+	}
+
+	void Update() {
+		Recorder.Update();
+		int l = TrajectoryTracks.Count;
+		if (l > 0) {
+			for (int i = 0; i < l; i++) {
+				TrajectoryTracks[i].Advance();
+			}
+		}
+	}
+
+	private void CreateNewTrack(Controller whichController) {
+		//create new recorded track
+		GameObject CurrentTrack = Instantiate(TrajectoryTrackPrefab, Vector3.zero, Quaternion.identity, TR) as GameObject;
+		if (whichController == Controller.Controller1) {
+			CurrentTrajectoryTrack1 = CurrentTrack.GetComponent<TrajectoryTrack>();
+		} else if(whichController == Controller.Controller2) {
+			CurrentTrajectoryTrack2 = CurrentTrack.GetComponent<TrajectoryTrack>();
+		}
+	}
+
+	private void TrackRecordFinished(Controller whichController) {
+		if (whichController == Controller.Controller1) {
+			CurrentTrajectoryTrack1.StartTrackPlayback();
+			//add current track to list
+			TrajectoryTracks.Add(CurrentTrajectoryTrack1);
+		}
+		if (whichController == Controller.Controller2) {
+			CurrentTrajectoryTrack2.StartTrackPlayback();
+			//add current track to list
+			TrajectoryTracks.Add(CurrentTrajectoryTrack2);
+		}
+	}
+
+	private void UpdateCurrentTrack(Controller whichController, Vector3 trackPoint) {
+		if (whichController == Controller.Controller1) {
+			CurrentTrajectoryTrack1.AddTrackPoint(trackPoint);
+		}
+		if (whichController == Controller.Controller2) {
+			CurrentTrajectoryTrack2.AddTrackPoint(trackPoint);
+		}
+	}
+
+}
