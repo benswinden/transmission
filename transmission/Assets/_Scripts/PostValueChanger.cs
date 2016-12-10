a3bda77d2222d92db98bb6c70ed26b90f5296e49
ben <swinden.ben@gmail.com>
2016-12-10 16:27:28 -0500

    : more final tweaks for the absentia video

---
+++ transmission/Assets/_Scripts/PostValueChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PostValueChanger : MonoBehaviour {
+
+    public float value = 0;
+
+    UnityEngine.PostProcessing.Utilities.PostProcessingController controller;
+
+    void Awake() {
+
+        controller = GetComponent<UnityEngine.PostProcessing.Utilities.PostProcessingController>();
+    }
+
+    void Update() {
+
+        controller.bloom.bloom.intensity = value;
+    }
+
+}
---
+++ transmission/Assets/_Scripts/Shell2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Shell2 : MonoBehaviour {
+
+
+    void Awake() {
+
+        StartCoroutine("routine");
+    }
+
+    void Update() {
+
+        GetComponent<ShellRenderer>().noiseAmplitude += 0.000042f;
+    }
+
+    IEnumerator routine() {
+
+        yield return new WaitForSeconds(120);
+
+        Destroy(this);
+    }
+}
